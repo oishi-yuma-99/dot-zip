94eff0eaca61c1243358119092c6756645c5a9e1
oishi-yuma-99 <138931627+oishi-yuma-99@users.noreply.github.com>
2024-04-09 14:42:52 +0000

    [WIP]Gem等初期設定

---
+++ app/controllers/public/accounts_controller.rb
@@ -0,0 +1,2 @@
+class Public::AccountsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admins, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
+# ユーザー用
+# URL /sign_in ...
+devise_for :users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
