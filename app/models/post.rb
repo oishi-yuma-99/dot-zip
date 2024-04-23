453871e5c2dad1356962de2e76604d37246c8a89
oishi-yuma-99 <138931627+oishi-yuma-99@users.noreply.github.com>
2024-04-23 13:17:59 +0000

    [Add]投稿コメント機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
   
   def self.search_for(content, method)
     Post.where('body LIKE ?', '%' + content + '%')
