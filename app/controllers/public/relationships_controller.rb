ed329112d3cd1196303f536147b85817bc12072d
oishi-yuma-99 <138931627+oishi-yuma-99@users.noreply.github.com>
2024-04-24 09:39:30 +0000

    [Add]フォロー機能実装

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,2 +1,25 @@
 class Public::RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    user = User.find_by(account_name: params[:user_account_name])
+    current_user.follow(user)
+    redirect_back(fallback_location: root_path)
+  end
+  
+  def destroy
+    user = User.find_by(account_name: params[:user_account_name])
+    current_user.unfollow(user)
+    redirect_back(fallback_location: root_path)
+  end
+  
+  def followings
+    @user = User.find_by(account_name: params[:user_account_name])
+    @users = @user.followings
+  end
+  
+  def followers
+    @user = User.find_by(account_name: params[:user_account_name])
+    @users = @user.followers
+  end
 end
